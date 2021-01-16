09075a92c60b3f5a9a033db65d05046e987787cb
MERCUROUS <64265450+MERCUROUS@users.noreply.github.com>
2021-01-16 17:39:11 +0530

    UX updated

--- app/src/main/java/com/bugslayers/quickattend/students/StudentProfile.java
+++ app/src/main/java/com/bugslayers/quickattend/students/StudentProfile.java
@@ -2,9 +2,16 @@ package com.bugslayers.quickattend.students;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 import com.bugslayers.quickattend.R;
+import com.bugslayers.quickattend.activity.NoticeListActivity;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class StudentProfile extends AppCompatActivity {
 
@@ -12,5 +19,29 @@ public class StudentProfile extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_student_profile);
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu)
+    {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.sign_out_menu, menu);
+        return true;
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId())
+        {
+            case R.id.sign_out_menu:
+            {
+                FirebaseAuth.getInstance().signOut();
+                Toast.makeText(com.bugslayers.quickattend.students.StudentProfile.this,"Sign out!",Toast.LENGTH_SHORT).show();
+                Intent transfer=new Intent(StudentProfile.this, StudentLoginActivity.class);
+                startActivity(transfer);
+                finish() ;
+            }
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
