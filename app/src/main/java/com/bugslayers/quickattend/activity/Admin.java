2ea2df0360e680bccf965c559d0b00980e418159
MERCUROUS <64265450+MERCUROUS@users.noreply.github.com>
2021-01-16 20:31:52 +0530

    UX updated

--- app/src/main/java/com/bugslayers/quickattend/activity/Admin.java
+++ app/src/main/java/com/bugslayers/quickattend/activity/Admin.java
@@ -12,22 +12,14 @@ import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.bugslayers.quickattend.R;
-import com.bugslayers.quickattend.admin.AdminLoginActivity;
-import com.bugslayers.quickattend.teachers.TeacherLoginActivity;
+
 
 public class Admin extends AppCompatActivity {
  Button addStudent;
  Button addTeacher;
 
-import android.widget.TextView;
 
-import com.bugslayers.quickattend.R;
-import com.bugslayers.quickattend.students.StudentLoginActivity;
-import com.bugslayers.quickattend.teachers.TeacherLoginActivity;
 
-public class Admin extends AppCompatActivity {
-Button addStudent;
-Button addTeacher;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,13 +27,13 @@ Button addTeacher;
         setContentView(R.layout.activity_admin);
 
 
-        Button addStudent = findViewById(R.id.add_student_btn);
-        addTeacher = findViewById(R.id.add_teacher_btn);
+        addStudent = findViewById(R.id.add_newStudent);
+        addTeacher = findViewById(R.id.add_newTeacher);
 
         addStudent.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent=new Intent(Admin.this, AdminStudentsInput.class);
+                Intent intent = new Intent(Admin.this, AdminStudentsInput.class);
                 startActivity(intent);
                 finish();
             }
@@ -50,27 +42,12 @@ Button addTeacher;
         addTeacher.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent=new Intent(Admin.this, AdminTeachersInput.class);
+                Intent intent = new Intent(Admin.this, AdminTeachersInput.class);
                 startActivity(intent);
                 finish();
 
             }
-        addStudent=findViewById(R.id.add_newStudent);
-        addTeacher=findViewById(R.id.add_newTeacher);
-
-        addStudent.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-                startActivity(new Intent(Admin.this, AdminStudentsInput.class));
-            }
-        });
-        addTeacher.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-//                startActivity( new Intent(Admin.this, TeacherLoginActivity.class));
-           }
-
         });
     }
+
 }
